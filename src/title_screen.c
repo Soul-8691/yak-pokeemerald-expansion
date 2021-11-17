d08adc6c4b62b27130229ad31452b66586f01334
Demonheadge <64343182+Demonheadge@users.noreply.github.com>
2021-11-17 11:07:19 +0800

    Updated music

--- src/title_screen.c
+++ src/title_screen.c
@@ -679,7 +679,7 @@ void CB2_InitTitleScreen(void)
                                     | DISPCNT_OBJ_ON
                                     | DISPCNT_WIN0_ON
                                     | DISPCNT_OBJWIN_ON);
-        m4aSongNumStart(MUS_TITLE);
+        m4aSongNumStart(MUS_PS_TITLESCREEN);
         gMain.state = 5;
         break;
     case 5:
